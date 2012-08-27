8f9090c7e33494590a176f285fba3dd78197340a
Oliver Lloyd <oliver_lloyd@hotmail.com>
2012-08-27 18:37:12 +0100

    Better defaults for empty DBs

--- projectstatus.js
+++ projectstatus.js
@@ -150,15 +150,16 @@ if (Meteor.is_client) {
 	};
 };
 
+
 // On server startup, create some projects if the database is empty. Left over from the example to show where server code would go if there were any!
 if (Meteor.is_server) {
 	Meteor.startup(function () {
 		if (Projects.find().count() === 0) {
-			var names = ["DEMO ACS",
-			"DEMO CRM",
-			"DEMO DRT"];
+			var names = ["DEMO PROJECT 1",
+			"DEMO PROJECT 2",
+			"DEMO PROJECT 3"];
 			for (var i = 0; i < names.length; i++)
-			Projects.insert({name: names[i], score: Math.floor(Math.random()*10)*5, status: "OK"});
+			Projects.insert({name: names[i], target: 100, status: "GOOD", release: "V0.1.2", comment: "Dummy comment test", risk: "It all might blow up"});
 		}
 	});
 }
